18881e68bd8d2ae7b75543deb6c8ddca61d5c546
Benjamin Blarr <b.blarr@gmx.de>
2025-11-01 16:52:49 +0100

    added feat like button and like counter, like button changes by click

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,37 @@
-
 const refArticle = document.getElementById("book-article");
 let currentIndex = 0;
 
 function renderBook() {
-   
-renderArticle()
-renderName();
-renderTable();
-priceTag();
-
-
-// render liked button und likes in content area
-// render comments in comment-area
-
-
+  renderArticle();
+  renderName();
+  renderTable();
+  priceTag();
+likeCounter()
+renderLikeButton();
+  // render liked button und likes in content area
+  // render comments in comment-area
 }
 function renderArticle() {
-    
-    for (let i = 0; i < books.length; i++) {
-    refArticle.innerHTML += getHtmlTemplate(i); 
-    }  
+  for (let i = 0; i < books.length; i++) {
+    refArticle.innerHTML += getHtmlTemplate(i);
+  }
 }
 
 function getHtmlTemplate(i) {
-    currentIndex = i;
-    return `<!--  headline-area-->
+  currentIndex = i;
+  return `<!--  headline-area-->
           <header class="book-headline"></header>
           <!--  book img-->
           <aside><img src="./assets/icons/favicon.png" alt="book" /></aside>
           <!--  content-area-->
           <section>
-            <div class="price-tag"></div>
+            <div class="price-button-area">
+                <div class="price-tag"></div>
+                <div class="like-area">
+                    <span class="likes-counter"></span>
+                    <button onclick=addLike(${i}) class="like-button"></button>
+                </div>
+            </div>
             <table>
               <tr>
                 <td>Autor</td>
@@ -45,8 +46,6 @@ function getHtmlTemplate(i) {
                 <td class="genre"></td>
               </tr>
             </table>
-            <span id="likes-counter"></span>
-            <button id="like-button"></button>
           </section>
           <!--  comments-area-->
           <section>
@@ -62,67 +61,123 @@ function getHtmlTemplate(i) {
             </div>
           </section>`;
 }
-  
 
 function renderName() {
-    let nameRef = document.getElementsByClassName("book-headline");
-    for (let i = 0; i < books.length; i++) {
-       nameRef[i].innerHTML += renderNameTemplate(i);
-    }
-
+  let nameRef = document.getElementsByClassName("book-headline");
+  for (let i = 0; i < books.length; i++) {
+    nameRef[i].innerHTML += renderNameTemplate(i);
+  }
 }
 function renderNameTemplate(i) {
-    return `<h2>${books[i].name}</h2>`; 
+  return `<h2>${books[i].name}</h2>`;
 }
 
-function renderTable(){
-renderAuthor();
-publishedYear();
-bookGenre();
+function renderTable() {
+  renderAuthor();
+  publishedYear();
+  bookGenre();
 }
 
 function renderAuthor() {
-   let authorRef = document.getElementsByClassName("author");
-    for (let i = 0; i < books.length; i++) {
-       authorRef[i].innerHTML += renderAuthorTemplate(i);
-    }
+  let authorRef = document.getElementsByClassName("author");
+  for (let i = 0; i < books.length; i++) {
+    authorRef[i].innerHTML += renderAuthorTemplate(i);
+  }
 }
 
 function renderAuthorTemplate(i) {
-    return `: ${books[i].author}`; 
+  return `: ${books[i].author}`;
 }
 
 function publishedYear() {
   let publishedYearRef = document.getElementsByClassName("published-year");
-    for (let i = 0; i < books.length; i++) {
-       publishedYearRef[i].innerHTML += renderPublishedYearTemplate(i);
-    }
+  for (let i = 0; i < books.length; i++) {
+    publishedYearRef[i].innerHTML += renderPublishedYearTemplate(i);
+  }
 }
 
 function renderPublishedYearTemplate(i) {
-    return `: ${books[i].publishedYear}`; 
+  return `: ${books[i].publishedYear}`;
 }
 
 function bookGenre() {
-    let genreRef = document.getElementsByClassName("genre");
-    for (let i = 0; i < books.length; i++) {
-       genreRef[i].innerHTML += bookGenreTemplate(i);
-    }
+  let genreRef = document.getElementsByClassName("genre");
+  for (let i = 0; i < books.length; i++) {
+    genreRef[i].innerHTML += bookGenreTemplate(i);
+  }
 }
 
 function bookGenreTemplate(i) {
-    return `: ${books[i].genre}`; 
+  return `: ${books[i].genre}`;
 }
 
 // ToDo Beim Preis Punkt durch Komma ersetzen
 function priceTag() {
-    let priceTagRef = document.getElementsByClassName("price-tag");
-    for (let i = 0; i < books.length; i++) {
-       priceTagRef[i].innerHTML += priceTagTemplate(i);
-    }
+  let priceTagRef = document.getElementsByClassName("price-tag");
+  for (let i = 0; i < books.length; i++) {
+    priceTagRef[i].innerHTML += priceTagTemplate(i);
+  }
 }
 
 function priceTagTemplate(i) {
-    return `: ${books[i].price} €`; 
+  return `: ${books[i].price} €`;
 }
 
+function likeCounter() {
+    let likeRef = document.getElementsByClassName("likes-counter");
+  for (let i = 0; i < books.length; i++) {
+    likeRef[i].innerHTML += likeCounterTemplate(i);
+  }
+}
+
+function likeCounterTemplate(i) {
+  return `${books[i].likes}`;
+}
+
+function renderLikeButton() {
+    let likeButtonRef = document.getElementsByClassName("like-button");
+  for (let i = 0; i < books.length; i++) {
+    likeButtonRef[i].innerHTML += likeButtonTemplate(i);
+  }
+}
+
+function likeButtonTemplate(i) {
+    if (books[i].liked == true){
+   return '<img src="./assets/icons/heart.png" alt="heart_icon">';
+} else
+{
+    return '<img src="./assets/icons/heart2.png" alt="heart_icon">';
+}
+}
+
+
+
+function addLike(j) {
+    let addLikeRef = document.getElementsByClassName("like-button");
+    addLikeRef[j].innerHTML = changeLikeTemplate(j);
+
+    let likeCounterRef = document.getElementsByClassName("likes-counter");
+    likeCounterRef[j] = changeLikeCounterTemplate(j);
+}
+
+function changeLikeTemplate(i) {
+    if (books[i].liked == true){
+        books[i].liked = false;
+   return '<img src="./assets/icons/heart2.png" alt="heart2_icon">';
+} else if  (books[i].liked == false)
+{ 
+    books[i].liked = true;
+    return '<img src="./assets/icons/heart.png" alt="heart_icon">';
+}
+}
+
+function changeLikeCounterTemplate(i) {
+    if (books[i].liked == true){
+     return books[i].likes = i + 1;
+   
+} else if  (books[i].liked == false)
+{ 
+   return books[i].likes = i - 1;
+    
+}
+}
